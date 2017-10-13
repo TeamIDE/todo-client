0801821ddc8c6a2b9af552158315a462d2a566ff
Will Shiel <ws248531@muhlenberg.edu>
2017-10-12 23:50:30 -0400

    Null check when loading todos

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,13 +3,19 @@ import TodoModel from './TodoModel.js';
 
 class TodoList extends Component {
     render() {
-        let todoNodes = this.props.data.map(todo => {
-            return (
-                <TodoModel name={ todo.name }>
-                    { todo.description }
-                </TodoModel>
-            )
-        })
+        var todoNodes;
+        console.log(this.props.data);
+        if (this.props.data) {
+            todoNodes = this.props.data.map(todo => {
+                return (
+                    <TodoModel name={ todo.name }>
+                        { todo.description }
+                    </TodoModel>
+                )
+            });
+        } else {
+            todoNodes = null;
+        }
         return (
             <div>
                 { todoNodes }
